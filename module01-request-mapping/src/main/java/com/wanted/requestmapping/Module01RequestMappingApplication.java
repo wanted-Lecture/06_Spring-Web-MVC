ce515e526e69bd6807df745ad0c85a6ec78c35c1
vinent-an <xxv211@naver.com>
2026-04-02 10:21:50 +0900

    기본 세팅

---
+++ module01-request-mapping/src/main/java/com/wanted/requestmapping/Module01RequestMappingApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.requestmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module01RequestMappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module01RequestMappingApplication.class, args);
+    }
+
+}
---
+++ module01-request-mapping/src/test/java/com/wanted/requestmapping/Module01RequestMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.requestmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module01RequestMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
